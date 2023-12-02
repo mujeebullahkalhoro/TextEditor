22ec59889ab48454854712df52e7cc42c33a83f9
Mujeebullah <mujeebullahkalhoro77@gmail.com>
2023-12-02 19:17:18 +0500

    underline feature on mispelled word is added

--- src/com/twostarts/TextPaneDocumentListener.java
+++ src/com/twostarts/TextPaneDocumentListener.java
@@ -19,15 +19,11 @@ class TextPaneDocumentListener implements DocumentListener {
     Trie trie = new Trie();
 
     private JTextPane textPane;
-    
 
     TextPaneDocumentListener(JTextPane textPane) {
         this.textPane = textPane;
 
-        
     }
-
-    
 
     @Override
     public void insertUpdate(DocumentEvent e) {
@@ -45,124 +41,129 @@ class TextPaneDocumentListener implements DocumentListener {
     }
 
     // private void updateUnderline(DocumentEvent e) {
-    //     SwingUtilities.invokeLater(() -> {
-    //         StyledDocument doc = textPane.getStyledDocument();
+    // SwingUtilities.invokeLater(() -> {
+    // StyledDocument doc = textPane.getStyledDocument();
 
-    //         // Remove underlining from the entire document
-    //         doc.setCharacterAttributes(0, doc.getLength(), new SimpleAttributeSet(), true);
+    // // Remove underlining from the entire document
+    // doc.setCharacterAttributes(0, doc.getLength(), new SimpleAttributeSet(),
+    // true);
 
-    //         try {
-    //             String text = doc.getText(0, doc.getLength());
-    //             int start = 0;
+    // try {
+    // String text = doc.getText(0, doc.getLength());
+    // int start = 0;
 
-    //             while (start < text.length()) {
-    //                 int index = text.indexOf(" ", start);
-    //                 if (index == -1) {
-    //                     index = text.length();
-    //                 }
+    // while (start < text.length()) {
+    // int index = text.indexOf(" ", start);
+    // if (index == -1) {
+    // index = text.length();
+    // }
 
-    //                 String word = text.substring(start, index);
-    //                 AttributeSet attrs = doc.getCharacterElement(start).getAttributes();
+    // String word = text.substring(start, index);
+    // AttributeSet attrs = doc.getCharacterElement(start).getAttributes();
 
-    //                 if (!StyleConstants.isUnderline(attrs) && !trie.search(word)) {
-    //                     // Only underline if it's not already underlined and the word is misspelled
-    //                     doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), false);
-    //                     doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), true);
-    //                 }
+    // if (!StyleConstants.isUnderline(attrs) && !trie.search(word)) {
+    // // Only underline if it's not already underlined and the word is misspelled
+    // doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(),
+    // false);
+    // doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(),
+    // true);
+    // }
 
-    //                 start = index + 1;
-    //             }
-    //         } catch (BadLocationException ex) {
-    //             ex.printStackTrace();
-    //         }
-    //     });
+    // start = index + 1;
+    // }
+    // } catch (BadLocationException ex) {
+    // ex.printStackTrace();
+    // }
+    // });
     // }
 
     private SimpleAttributeSet getUnderlineAttribute() {
         SimpleAttributeSet underline = new SimpleAttributeSet();
+        StyleConstants.setForeground(underline, Color.RED);
+        StyleConstants.setForeground(underline, Color.RED);
         StyleConstants.setUnderline(underline, true);
         return underline;
     }
+
     private void updateUnderline(DocumentEvent e) {
-    SwingUtilities.invokeLater(() -> {
-        StyledDocument doc = textPane.getStyledDocument();
-
-        // Remove underlining from the entire document
-        doc.setCharacterAttributes(0, doc.getLength(), new SimpleAttributeSet(), true);
-
-        try {
-            String text = doc.getText(0, doc.getLength());
-            int start = 0;
-
-            while (start < text.length()) {
-                int index = text.indexOf(" ", start);
-                if (index == -1) {
-                    index = text.length();
-                }
-
-                String word = text.substring(start, index);
-                AttributeSet attrs = doc.getCharacterElement(start).getAttributes();
-
-                if (!StyleConstants.isUnderline(attrs) && !trie.search(word)) {
-                    // Only underline if it's not already underlined and the word is misspelled
-                    doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), false);
-                    doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), true);
-
-                    // Add a right-click listener to show the suggestion popup
-                    textPane.addMouseListener(new SuggestionMouseListener(word));
-                }
-
-                start = index + 1;
-            }
-        } catch (BadLocationException ex) {
-            ex.printStackTrace();
-        }
-    });
-}
-
-private class SuggestionMouseListener extends MouseAdapter {
-    private final String misspelledWord;
-
-    public SuggestionMouseListener(String misspelledWord) {
-        this.misspelledWord = misspelledWord;
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (SwingUtilities.isRightMouseButton(e)) {
-            showSuggestionPopup(e.getComponent(), e.getX(), e.getY());
-        }
-    }
-
-    private void showSuggestionPopup(Component component, int x, int y) {
-        JPopupMenu popupMenu = new JPopupMenu();
-        ArrayList<String> suggestions = (ArrayList<String>) trie.getSuggestions(misspelledWord);
-
-        for (String suggestion : suggestions) {
-            JMenuItem menuItem = new JMenuItem(suggestion);
-            menuItem.addActionListener(actionEvent -> {
-                replaceMisspelledWord(misspelledWord, suggestion);
-                popupMenu.setVisible(false);
-            });
-            popupMenu.add(menuItem);
-        }
-
-        popupMenu.show(component, x, y);
-    }
-
-    private void replaceMisspelledWord(String misspelledWord, String replacement) {
-        try {
+        SwingUtilities.invokeLater(() -> {
             StyledDocument doc = textPane.getStyledDocument();
-            int start = doc.getText(0, doc.getLength()).indexOf(misspelledWord);
-            int end = start + misspelledWord.length();
-            
-            doc.remove(start, misspelledWord.length());
-            doc.insertString(start, replacement, null);
-        } catch (BadLocationException ex) {
-            ex.printStackTrace();
+
+            // Remove underlining from the entire document
+            doc.setCharacterAttributes(0, doc.getLength(), new SimpleAttributeSet(), true);
+
+            try {
+                String text = doc.getText(0, doc.getLength());
+                int start = 0;
+
+                while (start < text.length()) {
+                    int index = text.indexOf(" ", start);
+                    if (index == -1) {
+                        index = text.length();
+                    }
+
+                    String word = text.substring(start, index);
+                    AttributeSet attrs = doc.getCharacterElement(start).getAttributes();
+
+                    if (!StyleConstants.isUnderline(attrs) && !trie.search(word)) {
+                        // Only underline if it's not already underlined and the word is misspelled
+                        doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), false);
+                        doc.setCharacterAttributes(start, index - start, getUnderlineAttribute(), true);
+
+                        // Add a right-click listener to show the suggestion popup
+                        textPane.addMouseListener(new SuggestionMouseListener(word));
+                    }
+
+                    start = index + 1;
+                }
+            } catch (BadLocationException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    private class SuggestionMouseListener extends MouseAdapter {
+        private final String misspelledWord;
+
+        public SuggestionMouseListener(String misspelledWord) {
+            this.misspelledWord = misspelledWord;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (SwingUtilities.isRightMouseButton(e)) {
+                showSuggestionPopup(e.getComponent(), e.getX(), e.getY());
+            }
+        }
+
+        private void showSuggestionPopup(Component component, int x, int y) {
+            JPopupMenu popupMenu = new JPopupMenu();
+            ArrayList<String> suggestions = (ArrayList<String>) trie.getSuggestions(misspelledWord);
+
+            for (String suggestion : suggestions) {
+                JMenuItem menuItem = new JMenuItem(suggestion);
+                menuItem.addActionListener(actionEvent -> {
+                    replaceMisspelledWord(misspelledWord, suggestion);
+                    popupMenu.setVisible(false);
+                });
+                popupMenu.add(menuItem);
+            }
+
+            popupMenu.show(component, x, y);
+        }
+
+        private void replaceMisspelledWord(String misspelledWord, String replacement) {
+            try {
+                StyledDocument doc = textPane.getStyledDocument();
+                int start = doc.getText(0, doc.getLength()).indexOf(misspelledWord);
+                int end = start + misspelledWord.length();
+
+                doc.remove(start, misspelledWord.length());
+                doc.insertString(start, replacement, null);
+            } catch (BadLocationException ex) {
+                ex.printStackTrace();
+            }
         }
     }
-}
-
 
 }
